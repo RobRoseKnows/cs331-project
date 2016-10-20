2b2e6e26f64c217d1c4ee304f234ee01a17b13fc
SarahHeiner <she3@umbc.edu>
2016-10-20 16:31:27 -0400

    Update studentSignup.php

--- studentSignup.php
+++ studentSignup.php
@@ -6,26 +6,23 @@ include("CommonVariables.php");
 
 <form action='doStudentSignUp.php' method='post' name='studentSignUp'>
     <label for='studEmail'>Email: </label>
-    <input type='email' id='studEmail' pattern='<? echo $umbcEmailPattern; ?>' \
-required placeholder='example@umbc.edu'> <br>
+    <input type='email' id='studEmail' pattern='[A-Za-z0-9._+-]+@[A-Za-z0-9._-]*umbc.edu' required placeholder='example@umbc.edu'> <br>
 
   <label for='studIdNum'>Student Id Number: </label>
-    <input type='text' id='studIdNum' pattern='<? echo $umbcIdPattern;?>' requi\
-red> <br>
+    <input type='text' id='studIdNum' name='studIdNum' pattern='[A-Z]{2}[0-9]{5}' required> <br>
 
   <label for='fName'>First Name: </label>
-    <input type='text' id='fName' required> <br>
+    <input type='text' id='fName' name='fName' required> <br>
 
   <label for='lName'>Last Name: </label>
-    <input type='text' id='lName' required> <br>
+    <input type='text' id='lName' name='lName' required> <br>
 
   <label for='major'>Major: </label>
     <select name='major'>
         <option value='BioBA'>Biological Sciences BA</option>
         <option value='BioBS'>Biological Sciences BS</option>
         <option value='Biochem'>Biochemisty & Molecular Biology BS</option>
-        <option value='Bioinfo'>Bioinformatics & Computational Biology BS</opti\
-on>
+        <option value='Bioinfo'>Bioinformatics & Computational Biology BS</option>
         <option value='Bioed'>Biology Education BA</option>
         <option value='ChemBA'>Chemistry BA</option>
         <option value='ChemBS'>Chemistry BS</option>
@@ -33,13 +30,7 @@ on>
     </select> <br>
 
   <label for='password'>Password: </label>
-    <input type='password' id='password' required> <br>
+    <input type='password' id='password' name='password' required> <br>
 
   <label for='rePassword'>Retype Password: </label>
-    <input type='password' id='rePassword' required> <br>
-
-</form>
-
-<?php
-  require('footer.php');
-?>
+    <input type='password' id='rePassword' name='rePassword' required> <br>
