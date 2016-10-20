dcff6c5cc660b2f1e3164524f460fd0c2ab39ff0
SarahHeiner <she3@umbc.edu>
2016-10-20 16:37:35 -0400

    Create doStudentSignUp.php

---
+++ doStudentSignUp.php
@@ -0,0 +1,28 @@
+<?php
+
+include('CommonMethods.php');
+$debug = true;
+$COMMON = new Common($debug);
+
+//if the passwords given both match, create a new student
+if(($_POST['password']) == ($_POST['rePassword'])){
+
+  $studIdNum = ($_POST['studIdNum']);
+  $fName = ($_POST['fName']);
+  $lName = ($_POST['lName']);
+  $major = ($_POST['major']);
+  $password = ($_POST['password']);
+
+  $sql = "insert into `Student Data` (`ID`, `StudentID`, `FirstName`, `LastName`,`Password`, `ApptNum`,`Major`) values (NULL, '$studIdNum', '$fName', '$lName', '".md5($password)."', NULL, '$major')";
+
+
+  $rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
+}
+
+//otherwise, the passwords do not match and the user must be re-prompted
+else{
+
+  header('Location: studentSignup.php');
+
+}
+?>
