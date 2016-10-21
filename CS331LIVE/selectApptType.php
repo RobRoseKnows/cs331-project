6aeb68fb6718ca4e1c4b2289498c99fdc6b76647
Xytam <jhorvers97@gmail.com>
2016-10-21 16:30:15 -0400

    Jake's Current/Live Working
    directory for reference

---
+++ CS331LIVE/selectApptType.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+?>
+
+<html lang="en">
+<!--html styling-->
+
+  <head>
+    <meta charset="UTF-8" />
+    <title>Select Advising Type</title>
+	<link rel='stylesheet' type='text/css' href='./css/standard.css'/>
+  </head>
+  <body>
+    <div id="login">
+      <div id="form">
+        <div class="top">
+		<h1>Schedule Appointment</h1>
+		<h2>What kind of advising appointment would you like?</h2><br>
+
+	<!--determines which search to use based on input button-->
+	<form action="StudProcessType.php" method="post" name="SelectType">
+	<div class="nextButton">
+		<input type="submit" name="type" class="button large go" value="Individual">
+		<input type="submit" name="type" class="button large go" value="Group" style="float: right;">
+	    </div>
+		</div>
+		</form>
+
+
+<br>
+<br>
+		<div>
+		<!--Home buttons for the weary traveler-->
+		<form method="link" action="02StudHome.php">
+		<input type="submit" name="home" class="button large" value="Cancel">
+		</form>
+		</div>
+  </body>
+</html>
