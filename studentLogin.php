2d457b08432af216a115c58f22ed100f365e7e1b
SarahHeiner <she3@umbc.edu>
2016-10-24 14:38:21 -0400

    Update studentLogin.php

--- studentLogin.php
+++ studentLogin.php
@@ -5,6 +5,7 @@ require("header.php");
 
 <form action='logic/doStudentLogin.php' method='post' name='studentLogin'>
 
+   <!--gets information from student needed to authenticate login to their account-->
    <label for='studIdNum'>Id Number: </label>
      <input type='text' id='studIdNum' name='studIdNum' pattern=<?php echo("'$umbcIdPattern'"); ?> required> <br>
 
