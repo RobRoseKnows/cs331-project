d8dba033191d6bd7f8c9b849855287236fd04774
SarahHeiner <she3@umbc.edu>
2016-10-24 14:21:40 -0400

    Update advisorScheduleAppts.php

--- advisorScheduleAppts.php
+++ advisorScheduleAppts.php
@@ -8,6 +8,7 @@
 	<h1>Schedule Appointments</h1>
 	<h2>Select advising type</h2><br>
 
+	<!--select whether the appointment being created will be individual or group-->
 	<form method="post" action="./scheduleIndividual.php">
 	<div class="nextButton">
 		<input type="submit" name="next" class="button large go" value="Individual" style="float: center;">
@@ -17,6 +18,8 @@
 	<form method="post" action="./scheduleGroup.php">
 	<div class="nextButton">
 		<input type="submit" name="next" class="button large go" value="Group" style="float: center;">
+	
+	<!--advisor can also return to their homepage-->
 	</div>
 	</form><form method="link" action="advisorHome.php">
 			<input type="submit" name="home" class="button large" value="Home">
