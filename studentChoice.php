949753006ef19f9fda59aa2c23f764408d158adf
SarahHeiner <she3@umbc.edu>
2016-10-24 14:36:18 -0400

    Update studentChoice.php

--- studentChoice.php
+++ studentChoice.php
@@ -7,6 +7,7 @@
     <div id="login">
       <div id="form">
         <div class="top">
+	<!--brings student to page where they can create an account-->
         <form method="link" action="studentRegister.php">
 	<input type="submit" name="next" class="button large go" value="Student Sign Up">
 	</form>
@@ -14,6 +15,7 @@
 	<br />
 	<br />
 
+	<!--brings student to page where they can sign in if they already have an account-->
 	<form method="link" action="./studentSignIn.php">
 	<input type="submit" name="next" class="button large go" value="Returning Sign In">
 	</form>
