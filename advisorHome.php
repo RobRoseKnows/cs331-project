9f0008ac341098cd0a68a443aa3ba80a6c1871bf
SarahHeiner <she3@umbc.edu>
2016-10-24 14:15:08 -0400

    Update advisorHome.php

--- advisorHome.php
+++ advisorHome.php
@@ -7,6 +7,7 @@
 	<div id="nav">
 		<form action="redirects/redirectAdvisorHomepage.php" method="post" name="Home">
 	  
+			<!--options for advisor, will route to other pages-->
 			<input type="submit" name="next" class="button main selection" value="Schedule appointments"><br>
 			<input type="submit" name="next" class="button main selection" value="Print schedule for a day"><br>
 			<input type="submit" name="next" class="button main selection" value="Edit appointments"><br>
