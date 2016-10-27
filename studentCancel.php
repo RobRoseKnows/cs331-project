97c75806c76f129d51652be9fb420683a7044710
SarahHeiner <she3@umbc.edu>
2016-10-27 17:35:09 -0400

    Create studentCancel.php

--- studentCancel.php
+++ studentCancel.php
@@ -1,3 +1,22 @@
 <?php
-    //cancel appt file
+$title = "Cancel Appointment";
+require('./header.php');
 ?>
+
+<!-- message to user-->
+<h1><?php echo "Are you sure you want to cancel your appointment?";  ?> </h1>
+
+<nav>
+
+  <!--options to user-->
+  <a href="doStudentCancel.php">YES, cancel my appointment</a><br>
+  <a href="studentHome.php">NO, do not cancel my appointment</a><br>
+</nav>
+
+<?php
+require('./footer.php');
+?>
+
+
+
+
